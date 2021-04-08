19f1f68f661bda59ff57347dc587c050a1410ea6
c9at <81886392+c9at@users.noreply.github.com>
2021-04-08 09:13:16 +0800

    Update index.go

--- config/index.go
+++ config/index.go
@@ -26,12 +26,11 @@ enable_ssl: True
 handle_cookie: True
 
 host:
-  self: mirror.loerfy.now.sh
-  proxy: www.google.com
-
+  self: gogoo.vercel.app
+  proxy: startpage.com
 replaced_urls:
-  - old: www.google.com
-    new: mirror.loerfy.now.sh
+  - old: startpage.com
+    new: gogoo.vercel.app
 `
 var Config *Yaml
 var Protocal string
